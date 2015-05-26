4092f80ce28ec41c79b97991094d757966e8b471
Edward G Trenchard <ed_trench@yahoo.com>
2015-05-26 10:43:14 +0100

    1

---
+++ Thread1/Messenger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread1
+{
+    class Messenger
+    {
+
+        private readonly string _message;
+        private bool _cancel;
+
+        public Messenger(string message)
+        {
+            _message = message;
+            _cancel = false;
+        }
+
+        public void Cancel()
+        {
+            _cancel = true;
+        }
+
+        public void ShowMessage()
+        {
+            while (!_cancel)
+            {
+                Console.WriteLine(_message);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ Thread1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Thread1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var m = new Messenger("Hello Fred & Will");
+            Thread t = new Thread(m.ShowMessage);
+            t.Start();
+
+            Console.WriteLine("Press ENTER to cancel");
+            Console.ReadLine();
+
+            m.Cancel();
+            t.Join();
+
+            Console.WriteLine("Done!");
+            Console.ReadKey();
+        }
+    }
+}
